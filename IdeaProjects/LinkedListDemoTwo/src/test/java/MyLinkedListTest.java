f984688a687e177683a47267307ccff528104b5d
rksgr <r_kjha@yahoo.com>
2022-04-07 06:20:50 +0530

    Added: UC1 Create linked list containing three elements 56, 30 and 70

---
+++ IdeaProjects/LinkedListDemoTwo/src/test/java/MyLinkedListTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+public class MyLinkedListTest {
+
+    // UC2: Create a linked list by adding 56 and 30 to 70
+    @Test
+    public void given3NumbersWhenAddedToLinkedListShouldBeAddedToTop(){
+        MyNode<Integer> myNodeOne = new MyNode<>(70);
+        MyNode<Integer> myNodeTwo = new MyNode<>(30);
+        MyNode<Integer> myNodeThree = new MyNode<>(56);
+
+        
+    }
+}
---
+++ IdeaProjects/LinkedListDemoTwo/src/test/java/MyNodeTest.java
@@ -0,0 +1,24 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class MyNodeTest {
+
+
+    // UC1: create a linked list containing 56, 30 and 70 as elements
+    @Test
+    public void given3NumbersWhenLinkedShouldPassLinkedListTest(){
+        // Creation of three nodes
+        MyNode<Integer> myNodeOne = new MyNode<>(56);
+        MyNode<Integer> myNodeTwo = new MyNode<>(30);
+        MyNode<Integer> myNodeThree = new MyNode<>(70);
+
+        // Above nodes not linked together, we link them now
+        myNodeOne.setNext(myNodeTwo);
+        myNodeTwo.setNext(myNodeThree);
+
+        boolean result = myNodeOne.getNext().equals(myNodeTwo) &&
+                myNodeTwo.getNext().equals(myNodeThree);
+        Assert.assertTrue(result);
+    }
+}
